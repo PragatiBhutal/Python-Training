651cec71c502c80560d833f7612ba02fe1ce0c18
Pragati Bhutal <pragati.ishwara@annalect.com>
2024-12-16 19:42:43 +0530

    Added arithmetic operations: sum, difference, product, and division

---
+++ arithmetics.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    a = int(input("Enter the first integer: "))
+    b = int(input("Enter the second integer: "))
+      
+    sum_result = a + b
+    difference = a - b
+    product = a * b
+    integer_division = a // b
+    floating_division = a / b
+
+    print("Sum_result =",sum_result)
+    print("Diff_Result =",difference)
+    print("Product_Result =",product)
+    print("Division of Interger =",integer_division)
+    print("Division of Floating =", floating_division)
